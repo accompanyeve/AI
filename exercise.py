63768c0b8af51ecebc47e81d8109dfe51ace43b0
yangzhibin <yangzhibin@localhost.com>
2021-12-10 20:46:35 +0800

    temp12

--- exercise.py
+++ exercise.py
@@ -1,14 +1,51 @@
+from os import sep
 import pandas as pd
 # import numpy as np
-df = pd.read_csv("./testdata/pretreatment_train_dataset.csv")
-# df.drop(df.columns[[0,]], axis=1, inplace=True)
+df = pd.read_csv("./testdata/train_dataset.csv",sep='&',names=['ab'])
+tmp1 = pd.DataFrame()
+tmp2 = pd.DataFrame()
+i = 0
+t = 0
+# if i<10:
+#     t=0
+# print(df.iloc[0]['ab'][0:6])
+# print(df.iloc[22]['ab'][0:7])
+# print(df.iloc[111]['ab'][0:8])
+# print(df.iloc[1111]['ab'][0:9])
+
+#str = "by ."
+#print(df.iloc[101]['ab'][0:7])
+# def creat(t):
+#     print(t)
+# df[0].apply(creat)
+
+for i in range(9000):
+    if i<10:
+        t = 0
+    elif i < 100:
+        t = 1
+    elif i < 1000:
+        t = 2
+    else:
+        t = 3
+    if df.iloc[i]['ab'][0:6+t].replace("\t","") == str(str(i)+"by ."):
+        tmp1 = tmp1.append(df.iloc[i])
+    else:
+        tmp2 = tmp2.append(df.iloc[i])
+tmp1.to_csv("./testdata/tmp1.csv",sep="｜")
+tmp2.to_csv("./testdata/tmp2.csv",sep="｜")
+
+
+# print(df.iloc[0]['a'][2:6])
+#df.drop(df.columns[[1,]], axis=1, inplace=False)
+#df.drop(df.columns[[0,]], axis=1, inplace=True)
 # df = df['b'].str.split('.',3, expand = True).rename(columns={0:'d0', 1:'d1',2:'d2',3:'data'})
 # df.drop(df.columns[[0,1,2]], axis=1, inplace=True)
 # df = df.dropna()
-# df.to_csv("./testdata/pretreatment_train_dataset.csv",index=False,sep='|')
+# df.to_csv("./testdata/p_train_dataset.csv",index=False,sep='|')
 
 #df.drop(df.columns[[0,]], axis=1, inplace=True)
-print(df.head())
+
 # import pandas as pd
 
 # df = pd.DataFrame([
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print(df.iloc[11]['a'])
+# print("11"+df.iloc[11]['a'][1:6])
